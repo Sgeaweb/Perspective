a65bd4c1e3d22c1fead55dbe2ba8202c16277cad
sgeaweb <sgea@protonmail.com>
2019-11-18 07:56:23 -0500

    Add files

---
+++ script.js
@@ -0,0 +1,41 @@
+cDate = new Date();
+
+// Year
+birthYear = parseInt(prompt("Year of Birth", "1999"));
+currentYear = cDate.getFullYear();
+totalCurrentYear = currentYear * 365;
+yearDifference = currentYear - birthYear;
+totalYear = yearDifference * 365;
+
+// Month
+birthMonth = parseInt(prompt("Month of Birth", "1"));
+currentMonth = cDate.getMonth();
+monthDifference = currentMonth - birthMonth;
+totalMonth = monthDifference * 31;
+
+// Day
+birthDay = parseInt(prompt("Day of Birth", "1"));
+currentDay = cDate.getDate();
+dayDifference = currentDay - birthDay;
+totalDay = dayDifference + totalMonth + totalYear;
+
+// Future
+futureYear = (birthYear + 77) * 365;
+totalFutureDay = totalDay - totalYear + futureYear;
+futureCurrentDifference = totalFutureDay - totalCurrentYear;
+futureCurrentRemainder = totalFutureDay - futureCurrentDifference;
+
+// Show a number of blocks based on days.
+var i;
+for (i = 0; i <= futureCurrentDifference; i++) {
+	block = document.createElement("DIV");
+	block.className = "block";
+	document.body.appendChild(block);
+}
+
+var j;
+for (j = 0; j <= futureCurrentDifference; j++) {
+	empty = document.createElement("DIV");
+	empty.className = "empty";
+	document.body.appendChild(empty);
+}
